442dc5433152ebdec64f6fb19c2c3396e86cf73b
lwj <jg15575446613@163.com>
2024-01-03 09:47:19 +0800

    feat: learn

--- Types & Grammar/1.js
+++ Types & Grammar/1.js
@@ -45,13 +45,82 @@ if(window.DEBUG) {
 /**
  * --------------------------------------------- 值
  */
-var a = 'foo'
+/* var a = 'foo'
 var ret = Array.prototype.join.call(a, '.')
 console.log(ret);
-console.log(Array.prototype.map.call(a, (cha)=> cha.toUpperCase() + '.' ));
+console.log(Array.prototype.map.call(a, (cha)=> cha.toUpperCase() + '.' )); */
 
-console.log(Array.prototype.reverse.call(a));
+// console.log(Array.prototype.reverse.call(a)); // 改变数组的数组方法不能调用
 
+// Number.EPSION 表示1与大于1的最小浮点数之间的差值
+// 我理解的就是精度，我们把JS的数字范围比作一个刻度，0开始，第一个大于0的刻度为0.1
+// 那也就是说它的精度只能到0.1，那么0.1在JS中能够精确表示，但是0.01JS就不能精确表示
+// 比如我们有两个数，0.05 0.06,正常他们相加要等于0.11,但是由于精度只能到0.1,所以就可能导致0.05+0.06不等于0.11
+// 此时我们不能用===去判断两个数是否相等
+// 应该把0.5+06的和减去0.11,然后取绝对值，如果他们之间的差值在JS中最小差值只能，那就认为这两个数相等
+// 如果超过了，那就不相等
+
+/* function isNumberEqual(num1, num2) {
+  return Math.abs(num1 - num2) < Number.EPSILON
+}
+
+function isGraterThan(num1, num2) {
+  return num1 - num2 > Number.EPSILON
+}
+function isLessThan(num1, num2) {
+  return num2 - num1 > Number.EPSILON
+}
+
+// Number.isInteger 原理
+if(!Number.isInteger) {
+  Number.isInteger = function(value) {
+    return typeof value === 'number' && value % 1=== 0
+  }
+}
+
+// Number.isSafeInteger
+if(!Number.isSafeInteger) {
+  Number.isSafeInteger = function(value) {
+    // Math.abs 比如最大整数值为10，那么负数也就只能-10，所以此时要使用Math.abs
+    return Number.isInteger(value) && Math.abs(value) <= Number.MAX_SAFE_INTEGER
+  }
+} */
+
+
+var isReady = false
+function dosomething() {
+  if(!isReady) {
+    setTimeout(dosomething, 500);
+    return
+  }
+
+  return true
+}
+
+setTimeout(() => {
+  isReady = true
+}, 2000);
+
+// isNaN
+// 按道理应该是判断一个值是否为NaN
+isNaN(NaN) // true 符合预期
+isNaN('foo') // true 不符合预期
+
+// Number也有一个静态方法, Number.isNaN
+Number.isNaN(NaN)
+Number.isNaN('foo')
+
+// polyfill
+if(Number.isNaN) {
+  Number.isNaN = function(value) {
+    return typeof value === 'number' && isNaN(value)
+  }
+}
+
+// NaN 是JS中唯一一个不等于自身的值
+function isNaN(value) {
+  return value !== value
+}
 
 /**
  * --------------------------------------------- 类型

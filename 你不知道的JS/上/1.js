680462e1ff0e7a9e2e53b4085d211e69fc82ed5b
lwj <jg15575446613@163.com>
2024-03-19 18:35:22 +0800

    chore: learn

--- 你不知道的JS/上/1.js
+++ 你不知道的JS/上/1.js
@@ -34,7 +34,7 @@
 //   })(i)
 // }
 
-const myModules = (function defineModule() {
+/* const myModules = (function defineModule() {
   const _modules = {}
 
   function define(name, deps, fn) {
@@ -74,7 +74,7 @@ myModules.define('bar', ['foo'], function(foo) {
 const foo = myModules.getModule('foo')
 const bar = myModules.getModule('bar')
 
-bar.awesome()
+bar.awesome() */
 
 
 // var MyModules = (function Manager() {
@@ -118,4 +118,72 @@ bar.awesome()
 // ); // Let me introduce: hippo
 // foo.awesome(); // LET ME INTRODUCE: HIPPO
 
-// P70
+// this绑定规则
+// 1.默认绑定 独立函数调用
+/* function foo() {
+  console.log(this.a);
+}
+var a = 2
+foo() // 2 */
+// foo是直接使用不带任何修饰的函数引用进行调用的，因此只能是默认绑定，无法应用其他规则
+
+// 2.隐式绑定 调用位置是否有上下文对象
+// 对象属性引用中只有最后一层会影响调用位置
+/* function foo() {
+  console.log(this.a)
+}
+var obj2 = {
+  a: 42,
+  foo,
+}
+var obj1 = {
+  a: 2,
+  obj2
+}
+obj1.obj2.foo()  */// 42 foo的this指向最后一层的调用，也就是obj2
+// 隐式丢失
+/* function foo() {
+  console.log(this.a);
+}
+var obj = {
+  a: 2,
+  foo,
+}
+var bar = obj.foo
+var a = 'oops,global'
+bar() // oops,global bar其实只是引用了foo函数，然后调用的时候是不加任何修饰的调用，所以bar的this指向window
+ */
+// 3.显示绑定
+// function foo() {
+//   console.log(this.a);
+// }
+// var obj = {
+//   a: 2
+// }
+// foo.call(obj)
+
+// 4.new绑定
+// 使用new
+// 1.创建一个全新的对象
+// 2.这个新对象会被执行[[原型]]连接
+// 3.这个新对象会绑定到函数调用的this
+// 4.如果函数没有返回其他对象，那么new表达适中的函数调用会自动返回这个新对象
+function foo(a) {
+  this.a = a
+}
+const bar = new foo(2)
+console.log(bar.a);
+
+// new绑定优先级 > 显示绑定 > 隐式绑定 > 默认
+
+// 1. 函数是否在 new 中调用（new 绑定）？如果是的话 this 绑定的是新创建的对象。
+// var bar = new foo()
+// 2. 函数是否通过 call、apply（显式绑定）或者硬绑定调用？如果是的话，this 绑定的是
+// 指定的对象。
+// var bar = foo.call(obj2)
+// 3. 函数是否在某个上下文对象中调用（隐式绑定）？如果是的话，this 绑定的是那个上
+// 下文对象。
+// var bar = obj1.foo()
+// 4. 如果都不是的话，使用默认绑定。如果在严格模式下，就绑定到 undefined，否则绑定到
+// 全局对象。
+// var bar = foo()

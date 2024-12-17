c56862024a53709e03fee943c74a12ed6ac4bff4
lwj <jg15575446613@163.com>
2024-12-17 14:57:54 +0800

    learn: freeze问题

--- 你不知道的JavaScript/上/对象/2.js
+++ 你不知道的JavaScript/上/对象/2.js
@@ -1,21 +1,22 @@
+'use strict'
 // enumerable可枚举
-const objProto = {
-  'test-proto': '原型属性'
-}
+// const objProto = {
+//   'test-proto': '原型属性'
+// }
 
-var obj = Object.assign(Object.create(objProto), {
-  b: 2,
-  [Symbol(1)]: 'symbol'
-})
+// var obj = Object.assign(Object.create(objProto), {
+//   b: 2,
+//   [Symbol(1)]: 'symbol'
+// })
 
-Object.defineProperty(obj, 'a', {
-  value: 1,
-  writable: true,
-  configurable: true,
+// Object.defineProperty(obj, 'a', {
+//   value: 1,
+//   writable: true,
+//   configurable: true,
 
 
-  enumerable: true,
-})
+//   enumerable: true,
+// })
 
 // obj.propertyIsEnumerable('a')
 // console.log("🚀 ~ obj.propertyIsEnumerable('a'):", obj.propertyIsEnumerable('a'))
@@ -36,7 +37,7 @@ Object.defineProperty(obj, 'a', {
 /**
  * 对遍历的影响
  */
-Object.keys(obj)
+// Object.keys(obj)
 // Object.keys() 返回的是该对象自有的(Object.hasOwn())、可枚举的(enumerable)属性所组成的数组
 // console.log("🚀 ~ Object.keys(obj):", Object.keys(obj))
 
@@ -83,3 +84,112 @@ Object.keys(obj)
 //   ...obj
 // }
 // console.log("🚀 ~ obj2:", obj2)
+
+// Object.preventExtensions()
+// Object.
+// Object.isExtensible() 判断对象是否可以扩展
+// var objProto = {
+//   test: 2
+// }
+// var obj1 = Object.assign({
+//   b: 2
+// }, Object.create(objProto)) 
+// const obj2 = Object.preventExtensions(obj1)
+// console.log(obj1 === obj2)
+// 无法扩展，直接报错TypeError
+// Object.defineProperty(obj1, 'a', {
+//   value: 1
+// })
+// Object.defineProperty(obj2, 'a', {
+//   value: 1
+// })
+
+// Object.isExtensible(obj1)+
+
+// 调用preventExtensions()后，Object.isExtensible()返回false
+// console.log("🚀 ~ Object.isExtensible(obj1):", Object.isExtensible(obj1))
+
+// 严格模式下添加新属性也会报错 TypeError
+// obj1.a = '1'
+// 修改已有的属性是可以的
+// obj1.b = 3
+// 使用Object.defineProperty()修改已有属性的属性描述符是可以的
+// Object.defineProperty(obj2, 'b', {
+//   // value: 3,
+//   // writable: false,
+//   get() {
+//     return 1
+//   },
+//   // set() {
+//   //   return true
+//   // },
+//   enumerable: true,
+//   configurable: true
+// })
+
+// console.log("🚀 ~ obj2:", obj2)
+
+
+// 不能修改禁止扩展对象的原型， __proto__
+// obj1.__proto__ = {}
+
+// 非对象参数原样返回,不会报错
+// const oValue = Object.preventExtensions(1);
+
+// console.log("🚀 ~ oValue:", oValue)
+
+// 对象被Object.preventExtensions()后
+// 1.Object.isExtensible()为false
+// 2.不能添加新属性，直接新增或者通过Object.defineProperty()都是不可以的
+// 3.但是可以修改属性值 (直接修改或者通过Object.defineProperty修改都是可以的)
+// 4.对象的原型指向是不能修改的，obj.__proto__指向不能修改
+// 5.传入非对象不会报错，而是返回非对象值(也就是原始类型)
+
+
+// Object.seal(), 返回被密封的对象
+// 等价于对对象调用Object.preventExtensions(),并且将现有的属性描述符的configurable改为false
+// const obj = {
+//   a: 1
+// }
+// // 如果已有属性的configurable为false，调用seal也是不会报错的
+// Object.defineProperty(obj ,'b', {
+//   value: 2,
+//   configurable: false
+// })
+// Object.seal(obj)
+// Object.isExtensible(obj)
+// Object.getOwnPropertyDescriptors(obj)
+
+// console.log("🚀 ~ Object.getOwnPropertyDescriptors(obj):", Object.getOwnPropertyDescriptors(obj))
+
+
+// Object.freeze()
+// 等价于对对象调用Object.preventExtensions(),然后把现有的属性描述符的writable设置为false
+// 如果使用Object.defineProperty()定义时使用访问器描述符，那么还是可以修改该属性的值
+const obj = {
+  a: 1
+}
+
+
+var value = 1
+Object.defineProperty(obj, 'b', {
+  get() {
+    return value
+  },
+  set(nVal) {
+    value = nVal
+    return true
+  },
+})
+Object.freeze(obj)
+
+console.log("🚀 ~ obj:", Object.getOwnPropertyDescriptors(obj))
+obj.b = 2
+console.log("🚀 ~ obj:", obj.b)
+
+// 会报错，因为writable为false
+obj.a = 4
+
+
+
+

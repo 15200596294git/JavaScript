a10f0918c1622bb6f39c8ad3f689fc39ff2f4d2f
lwj <jg15575446613@163.com>
2024-12-23 22:50:13 +0800

    learn: 面向对象学习

--- 你不知道的JavaScript/上/类/1.js
+++ 你不知道的JavaScript/上/类/1.js
@@ -1,54 +1,61 @@
 // 农场有头大母牛，每年生头小母牛，小母牛五年后生小母牛，年龄大于15便死亡 ，问20年后农场一共有多少头牛？
 
-// 面向过程，描述解决问题的每一步
-function cow() {
-  const list = [5]
+// 
+function recursion(cows, years) {
+  if(years === 0) return cows
 
-  for(i = 0; i < 20; i ++) {
-    list.forEach((age, index)=> {
-      if(age >= 5 && age <= 15) list.push(1)
+  const _cows = cows.reduce((ret, cure)=> {
+    if(cure >=5) ret.push(1)
+    const afterAge = cure + 1
+    if(afterAge <= 15) ret.push(afterAge)
 
-      list[index]++
-    })
-    console.log(`第${i + 1}年, 牛的数量${list.filter(age=> age < 15).length}`)
-    // console.log(list)
-  }
+    return ret
+  }, [])
+
+  return recursion(_cows, years - 1)
 }
 
-cow()
+// recursion([5], 1)
+// console.log("🚀 ~ recursion([5], 1):", recursion([5], 20).length) // 20年后还有396头牛
+
 
 
 // 面向对象
-// 牛 年龄 存活状态 ，农场,牛的总数
-
 class Cow {
   constructor() {
     this.age = 1
   }
-  // 自然增长
-  plusOneAge() {
-    this.age++
+  getAge() {
+    return this.age
   }
-  // 直接设置年龄
   setAge(age) {
     this.age = age
   }
+  plusOneAge() {
+    this.age++
+  }
   isAlive() {
     return this.age <= 15
+  }
+  // 是否成年
+  isAdult() {
+    return this.age >= 5
   }
 }
 
 class Farm {
   constructor() {
-    /** @type {Cow[]} */
     this.cows = []
+  }
+  getCows() {
+    return this.cows
   }
   addCow(cow) {
     this.cows.push(cow)
   }
-  // 获取活着的牛的数量
-  getNumberOfAlive() {
-    return this.cows.filter(cow=> cow.isAlive()).length
+  // 获得活着的牛
+  getCowsOfAlive() {
+    return this.cows.filter(cow=> cow.isAlive())
   }
 }
 
@@ -57,11 +64,13 @@ const firstCow = new Cow()
 firstCow.setAge(5)
 farm.addCow(firstCow)
 
-for(let i = 0; i < 20; i ++) {
-  farm.cows.forEach(cow=> {
-    if(cow.age >= 5 && cow.isAlive()) farm.addCow(new Cow())
+for(let i = 0; i < 20; i++) {
+  farm.getCows().forEach(cow=> {
+    if(cow.isAdult() && cow.isAlive()) farm.addCow(new Cow())
+    
+    
     cow.plusOneAge()
   })
 }
 
-console.log(farm.getNumberOfAlive())
+console.log(farm.getCowsOfAlive().length)
